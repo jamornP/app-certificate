23d0ee2d0e429b6a6b6a12a1bd56a98273d9ae20
jamornP <100953989+jamornP@users.noreply.github.com>
2023-08-20 22:25:44 +0700

    up scicertificate head web

--- backend/pages/table.php
+++ backend/pages/table.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Project</title>
+    <title>Science-Certificate</title>
     <!-- link -->
     <?php require_once($_SERVER['DOCUMENT_ROOT'] . "/app-certificate/backend/component/link.php");   ?>
 
