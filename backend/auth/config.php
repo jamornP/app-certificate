9bad3d9973b63464eabcb5b1c46438c90a060bb8
jamornP <100953989+jamornP@users.noreply.github.com>
2023-08-24 18:12:48 +0700

    แก้่ไข เงื่อนไข session google login

--- backend/auth/config.php
+++ backend/auth/config.php
@@ -1,15 +1,19 @@
 <?php
+    session_start();
     require_once($_SERVER['DOCUMENT_ROOT'] .'/app-certificate/google-api/vendor/autoload.php');
     $gClient = new Google_Client();
     $gClient->setClientId("584689131463-qgtl775eq4d7ql9murb4kchhudr0lck4.apps.googleusercontent.com");
     $gClient->setClientSecret("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");
     $gClient->setApplicationName("App-Certificate");
-    if($_SESSION['linux']){
-        $gClient->setRedirectUri("http://sciserv01.sci.kmitl.ac.th/app-certificate/backend/auth/controller.php");
+    if(isset($_SESSION['linux'])){
+        if($_SESSION['linux']){
+            $gClient->setRedirectUri("http://sciserv01.sci.kmitl.ac.th/app-certificate/backend/auth/controller.php");
+        }else{
+            $gClient->setRedirectUri("http://localhost/app-certificate/backend/auth/controller.php");
+        }
     }else{
-        $gClient->setRedirectUri("http://localhost/app-certificate/backend/auth/controller.php");
+        $gClient->setRedirectUri("http://sciserv01.sci.kmitl.ac.th/app-certificate/backend/auth/controller.php");
     }
-    
     $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 
     $login_url = $gClient->createAuthUrl();
