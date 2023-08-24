c98e1a0ff02b7198d33e988585d578668731f300
jamornP <100953989+jamornP@users.noreply.github.com>
2023-08-24 18:09:28 +0700

    no up ck linux google login

--- backend/auth/config.php
+++ backend/auth/config.php
@@ -4,7 +4,12 @@
     $gClient->setClientId("584689131463-qgtl775eq4d7ql9murb4kchhudr0lck4.apps.googleusercontent.com");
     $gClient->setClientSecret("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");
     $gClient->setApplicationName("App-Certificate");
-    $gClient->setRedirectUri("http://sciserv01.sci.kmitl.ac.th/app-certificate/backend/auth/controller.php");
+    if($_SESSION['linux']){
+        $gClient->setRedirectUri("http://sciserv01.sci.kmitl.ac.th/app-certificate/backend/auth/controller.php");
+    }else{
+        $gClient->setRedirectUri("http://app.science.kmitl.ac.th/app-certificate/backend/auth/controller.php");
+    }
+    
     $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 
     $login_url = $gClient->createAuthUrl();
