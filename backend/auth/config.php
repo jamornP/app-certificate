c060f01676a307cc8bd679e59374d1489298cb0a
jamornP <100953989+jamornP@users.noreply.github.com>
2023-08-24 18:10:03 +0700

    localhos google login

--- backend/auth/config.php
+++ backend/auth/config.php
@@ -7,7 +7,7 @@
     if($_SESSION['linux']){
         $gClient->setRedirectUri("http://sciserv01.sci.kmitl.ac.th/app-certificate/backend/auth/controller.php");
     }else{
-        $gClient->setRedirectUri("http://app.science.kmitl.ac.th/app-certificate/backend/auth/controller.php");
+        $gClient->setRedirectUri("http://localhost/app-certificate/backend/auth/controller.php");
     }
     
     $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
